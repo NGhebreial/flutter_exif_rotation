33ae529e176b3c6cca61148b3f7afe58a1b1f884
itsmejohndoe <me@johndoe.dev>
2020-06-07 14:00:48 -0300

    Some bug fixes on iOS, changed the way we handle hint on textfields, minor fixes and changed the splash image.

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/nadia/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/nadia/AndroidStudioProjects/flutter_exif_rotation/example"
+export "FLUTTER_ROOT=/Users/john.lina/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/john.lina/Development/flutter_exif_orientation/flutter_exif_rotation/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/nadia/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/john.lina/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
